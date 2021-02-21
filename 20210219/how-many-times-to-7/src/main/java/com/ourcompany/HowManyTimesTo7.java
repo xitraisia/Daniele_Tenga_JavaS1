ebd7b7d33e5482261cc0225190c9e483e3d78932
Daniele Tenga <cookiie@Danieles-Air-2.attlocal.net>
2021-02-20 21:31:51 -0500

    how many times 7

--- 20210219/how-many-times-to-7/src/main/java/com/ourcompany/HowManyTimesTo7.java
+++ 20210219/how-many-times-to-7/src/main/java/com/ourcompany/HowManyTimesTo7.java
@@ -9,22 +9,23 @@ public class HowManyTimesTo7 {
         int myDieRoll2;
         int rolls;
         int counter = 0;
-        int numberLoops = 0;
+        int firstSeven = 0;
         Random randomGenerator = new Random();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             myDieRoll1 = randomGenerator.nextInt(6) + 1;
             myDieRoll2 = randomGenerator.nextInt(6) + 1;
             rolls = myDieRoll1 + myDieRoll2;
             System.out.println(rolls);
 
-            numberLoops++;
-
             if (rolls == 7) {
                 counter++;
+                if(firstSeven == 0){
+                    firstSeven = i;
+                }
             }
         }System.out.println("This is how many times you rolled a 7 " + counter);
-         System.out.println("This is how many times it took us to roll a 7 " + numberLoops);
+         System.out.println("This is how many times it took us to roll a 7: " + firstSeven);
     }
 }
 
