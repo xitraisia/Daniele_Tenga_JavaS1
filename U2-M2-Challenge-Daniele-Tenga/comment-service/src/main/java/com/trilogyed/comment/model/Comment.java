a02e520b4630fa34cf43c00370ceba6e7733cc03
Daniele Tenga <cookiie@Danieles-Air-2.attlocal.net>
2021-04-20 20:49:38 -0400

    created new folder for summative

---
+++ U2-M2-Challenge-Daniele-Tenga/comment-service/src/main/java/com/trilogyed/comment/model/Comment.java
@@ -0,0 +1,44 @@
+package com.trilogyed.comment.model;
+
+import java.time.LocalDate;
+
+public class Comment {
+
+    private int commentId;
+    private int postId;
+    private LocalDate createDate;
+    private String commenterName;
+    private String comment;
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCommenterName() {
+        return commenterName;
+    }
+
+    public void setCommenterName(String commenterName) {
+        this.commenterName = commenterName;
+    }
+}
